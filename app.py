cba3f50b4aa0fc60791ccd95da5636e76bc9b685
johanalain11 <jank092016@gmail.com>
2025-01-20 18:18:26 +0100

    UPDATE App.py

--- app.py
+++ app.py
@@ -23,30 +23,34 @@ def page3():
     frameP2.pack_forget()
     frameP1.pack_forget()
 
-
+# PREMIERE PAGE
 frameP1 = Frame(window)
 
 labelP1 = Label(frameP1, text="Vatecedi", font=("Algerian", 25))
+subLabelP1= Label(frameP1, text="Valeurs de tendance centrale et de dispersion", font=f)
 labelP1.pack(expand=True, fill=BOTH)
-button1P1 = Button(frameP1, text="Variable discrète", font=f, command=page2)
-button1P1.pack(fill=X, side=LEFT, padx=40)
-button2P1= Button(frameP1, text="Variable continue", font=f, command=page3)
-button2P1.pack(fill=X, side=RIGHT, padx=40)
+subLabelP1.pack(expand=True, fill=BOTH)
+button1P1 = Button(frameP1, text="Variables discrètes", font=f, command=page2)
+button1P1.pack(fill=BOTH, side=LEFT, pady=40)
+button2P1= Button(frameP1, text="Variables continues", font=f, command=page3)
+button2P1.pack(fill=BOTH, side=RIGHT, pady=40)
 
 frameP1.pack(expand=True, fill=BOTH)
 
+# PAGE NUMERO 2
 frameP2 = Frame(window)
 
-labelP2 = Label(frameP2, text="Valeurs de tendance centrale et de dispersion", font=("Algerian", 23))
+labelP2 = Label(frameP2, text="Vatecedi", font=("Algerian", 23))
 subLabelP2= Label(frameP2, text="Variables discrètes", font=f)
 labelP2.pack(expand=True, fill=BOTH)
 subLabelP2.pack(expand=True, fill=BOTH)
 buttonP2= Button(frameP2, text="Retour", font=f, command=page1)
 buttonP2.pack(fill=X, side=BOTTOM, pady=40)
 
+# PAGE NUMERO 3
 frameP3 = Frame(window)
 
-labelP3 = Label(frameP3, text="Valeurs de tendance centrale et de dispersion", font=("Algerian", 23))
+labelP3 = Label(frameP3, text="Vatecedi", font=("Algerian", 23))
 subLabelP3= Label(frameP3, text="Variables continues", font=f)
 labelP3.pack(expand=True, fill=BOTH)
 subLabelP3.pack(expand=True, fill=BOTH)
